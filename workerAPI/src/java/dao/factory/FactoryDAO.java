c71eb942a76395166ff6f014976a35800df0b5f5
Henrique Merlin <henmerlin@gmail.com>
2018-01-04 18:53:12 -0200

    add init files

---
+++ workerAPI/src/java/dao/factory/FactoryDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao.factory;
+
+import dao.http.HttpDAO;
+import dao.http.HttpDAOImpl;
+
+
+/**
+ *
+ * @author G0042204
+ */
+public class FactoryDAO {
+
+    public static HttpDAO createHttpDAO(){
+        return new HttpDAOImpl();
+    }
+
+}
